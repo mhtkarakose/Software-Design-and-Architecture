b3d146c6ee43114033aad2cc8f858b4c13869fa3
Mehmet KARAKÖSE <mhtkarakose@gmail.com>
2017-11-11 03:33:03 +0300

    Observer Pattern
    using observer pattern in java, source
    https://www.udemy.com/c-sharp-design-patterns/learn/v4/overview

---
+++ src/ObserverPattern/Observer.java
@@ -0,0 +1,66 @@
+package ObserverPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Observer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CustomerObserver ali = new CustomerObserver();
+		
+		ProductManager productManager = new ProductManager();
+		productManager.Attach(ali);
+		productManager.Attach(new EmployeeObserver());
+		productManager.Detach(ali);
+		productManager.UpdatePrice();
+ 
+	}
+
+}
+
+class ProductManager{
+	
+	List<AbstractObserver> _observers = new ArrayList<AbstractObserver>(); 
+	
+	public void UpdatePrice() {
+		System.out.println("fiyat güncellendi");
+		Notify();
+	} 
+	
+	public void Attach(AbstractObserver o ) {
+		_observers.add(o);
+	}
+	
+	public void Detach(AbstractObserver o) {
+		_observers.remove(o);
+	}
+	
+	private void Notify() {
+		_observers.forEach(item -> item.Update());
+	}
+}
+
+abstract class AbstractObserver{
+	public abstract void Update();
+}
+
+class CustomerObserver extends AbstractObserver{
+
+	@Override
+	public void Update() {
+		System.out.println("Message to customer: Product price change ");
+		 
+	}
+	
+}
+
+class EmployeeObserver extends AbstractObserver{
+
+	@Override
+	public void Update() {
+		System.out.println("Message to employee: Product price change");
+	}
+	
+}
